0551893eeb7619c892dfc19012fac9364441e1d0
framlin <w.egger@framlin.de>
2012-04-06 12:24:19 +0200

    log x-forward-for, too

--- server/run.js
+++ server/run.js
@@ -5,9 +5,18 @@ var ecstatic = require('ecstatic');
 app = new flatiron.App();
 app.use(flatiron.plugins.http);
 
+function getIP(req) {
+	return {
+		ip: ( req.headers["X-Forwarded-For"]
+		|| req.headers["x-forwarded-for"]
+		|| req.connection.remoteAddress )
+	};
+};
+
+
 app.http.before = [
    function (req, res) {
-	   app.log.info("REQ - " + new Date() + ' :: ' + req.connection.remoteAddress );
+	   app.log.info("REQ - " + new Date() + ' :: ' + getIP(req).ip );
 	   res.emit('next');
    },
    ecstatic(__dirname + '/../site')
