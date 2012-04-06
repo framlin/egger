e33416db8d0ce79dd9406885df85971eadc1e269
framlin <w.egger@framlin.de>
2012-04-06 11:17:08 +0200

    try some logging

--- server/run.js
+++ server/run.js
@@ -6,8 +6,12 @@ app = new flatiron.App();
 app.use(flatiron.plugins.http);
 
 app.http.before = [
-  ecstatic(__dirname + '/../site')
+   function (req, res) {
+	   //this logs >> info: REQ - hello clientundefined
+	   app.log.info("REQ - hello client" + req.socket);
+	   res.emit('next');
+   },
+   ecstatic(__dirname + '/../site')
 ];
 
 app.start(8081);
-
