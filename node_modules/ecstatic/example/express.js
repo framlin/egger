0c987d7549e7310b7c7c8e12228111ce837424bb
framlin (Wolfgang Egger) <w.egger@framlin.de>
2014-11-04 19:12:49 +0100

    json-data, content-fixes

--- node_modules/ecstatic/example/express.js
+++ node_modules/ecstatic/example/express.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var ecstatic = require('../lib/ecstatic');
+var http = require('http');
 
-var app = express.createServer();
-app.use(ecstatic(__dirname + '/public', { showdir : true }));
-app.listen(8080);
+var app = express();
+app.use(ecstatic({
+  root: __dirname + '/public',
+  showdir : true
+}));
+http.createServer(app).listen(8080);
 
 console.log('Listening on :8080');
--- node_modules/flatiron/node_modules/director/lib/director/http/responses.js
+++ node_modules/flatiron/node_modules/director/lib/director/http/responses.js
@@ -5,67 +5,88 @@ var util = require('util');
 
 exports.NotModified = function () {
   this.status = 304;
-  this.options = { 
+  this.options = {
     removeContentHeaders: true
-  };  
+  };
 };
 
 util.inherits(exports.NotModified, Error);
 
 exports.BadRequest = function (msg) {
+  msg = msg || 'Bad request';
+
   this.status = 400;
   this.headers = {};
+  this.message = msg;
   this.body = { error: msg };
 };
 
 util.inherits(exports.BadRequest, Error);
 
 exports.NotAuthorized = function (msg) {
+  msg = msg || 'Not Authorized';
+
   this.status = 401;
   this.headers = {};
-  this.body = { error: msg || 'Not Authorized' };
+  this.message = msg;
+  this.body = { error: msg };
 };
 
 util.inherits(exports.NotAuthorized, Error);
 
 exports.Forbidden = function (msg) {
+  msg = msg || 'Not Authorized';
+
   this.status = 403;
   this.headers = {};
-  this.body = { error: msg || 'Not Authorized' };
+  this.message = msg;
+  this.body = { error: msg };
 };
 
 util.inherits(exports.Forbidden, Error);
 
 exports.NotFound = function (msg) {
+  msg = msg || 'Not Found';
+
   this.status = 404;
   this.headers = {};
+  this.message = msg;
   this.body = { error: msg };
 };
 
 util.inherits(exports.NotFound, Error);
 
 exports.MethodNotAllowed = function (allowed) {
+  var msg = 'method not allowed.';
+
   this.status = 405;
   this.headers = { allow: allowed };
-  this.body = { error: "method not allowed." };
+  this.message = msg;
+  this.body = { error: msg };
 };
 
 util.inherits(exports.MethodNotAllowed, Error);
 
 exports.NotAcceptable = function (accept) {
+  var msg = 'cannot generate "' + accept + '" response';
+
   this.status = 406;
   this.headers = {};
+  this.message = msg;
   this.body = {
-    error: "cannot generate '" + accept + "' response",
-    only: "application/json"
+    error: msg,
+    only: 'application/json'
   };
 };
 
 util.inherits(exports.NotAcceptable, Error);
 
 exports.NotImplemented = function (msg) {
+  msg = msg || 'Not Implemented';
+
   this.status = 501;
   this.headers = {};
+  this.message = msg;
   this.body = { error: msg };
 };
 
